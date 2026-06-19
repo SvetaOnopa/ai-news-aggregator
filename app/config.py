16e2a90096828ae44dad22ce7468173f5aa5558e
Svetlana Onopa <onopasveta@gmail.com>
2026-06-19 21:10:24 +0200

    Customize sources and user profile

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 YOUTUBE_CHANNELS = [
     # "UCn8ujwUInbJkBhffxqAPBVQ", # Dave Ebbelaar
     "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
+    "UC5l7RouTQ60oUjLjt1Nh-UQ", # AI Revolution
+    "UCKelCK4ZaO6HeEI1KQjqzWA", # AI Daily Brief
+    "UCDZfn935vwaahDabsfylfIQ" # AI News Official 
 ]
 
